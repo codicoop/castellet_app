df57e9f0942f259906a6a162376caba19e951a69
eliashz <eliashz@gmail.com>
2024-05-14 10:34:10 +0200

    Añadir login_required en document_list_view

--- src/apps/main/views.py
+++ src/apps/main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from apps.main.forms import NewsletterForm
@@ -23,6 +24,7 @@ class NewsletterSuccessView(StandardSuccess):
     description = "Newsletter creada reeixidament."
 
 
+@login_required
 def document_list_view(request):
     if request.method == "GET":
         context = {
