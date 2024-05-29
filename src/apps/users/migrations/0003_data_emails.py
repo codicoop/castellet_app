dc3620befc7eb177e7eb73045da8ac5e3769142c
Pere Picornell <pere@perepicornell.com>
2024-05-29 14:54:39 +0200

    Textos emails arreglats

--- src/apps/users/migrations/0003_data_emails.py
+++ src/apps/users/migrations/0003_data_emails.py
@@ -58,7 +58,7 @@ ben llarga i t'agrairem que ens informis de la situació.
             id="email_verification",
             translated_templates={
                 "en": {
-                    "subject": "Email verification code for your account at " "{{project_name}}",
+                    "subject": "Email verification code for your account at {{project_name}}",
                     "body": """
     <p>Hello {{user_name}}!</p>
     <p>We're sending you this e-mail because today {{date}} at {{time}}
@@ -74,17 +74,18 @@ ben llarga i t'agrairem que ens informis de la situació.
                         """,
                 },
                 "ca": {
-                    "subject": "Verificació per correu electrònic del vostre compte a " "{{project_name}}",
+                    "subject": "Verificació del correu electrònic a {{project_name}}",
                     "body": """
     <p>Hola {{user_name}}!</p>
-    <p>Us enviarem aquest correu electrònic perquè avui {{date}} a les {{time}}
-        heu sol·licitat verificar el vostre correu electrònic {{user_email}}
+    <p>T'enviem aquest correu electrònic perquè avui {{date}} a les {{time}}
+        has sol·licitat verificar el teu correu electrònic {{user_email}}
         per a {{absolute.url}}.</p>
-    <p>Per a completar aquesta acció, introduïu el codi {{user_code}} fent clic
-        en el següent enllaç <a href="{{email_verification_url}}">{{email_verification_url}}</a>
+    <p>Per completar aquesta acció, introduïu el codi {{user_code}} a la pantalla
+    de verificació del correu, o bé cliqueu el següent enllaç:
+    <a href="{{email_verification_url}}">{{email_verification_url}}</a>
     </p>
     <p>Si no has estat tu qui ho ha demanat, ignora aquest missatge.</p>
-                        """,
+                    """,
                 },
             },
         ),
