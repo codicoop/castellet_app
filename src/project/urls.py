1f00f12113eda7921b47d7a80fc9476679cd6811
Pere Picornell <pere@perepicornell.com>
2024-09-03 15:06:08 +0200

    URLs wagtail afegides.

--- src/project/urls.py
+++ src/project/urls.py
@@ -14,14 +14,17 @@ Including another URLconf
 """
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.utils.translation import gettext_lazy as _
+from wagtail import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 from apps.main.views import NewsletterSubscriberSuccessView, newsletter_view
 from project.views import RootRedirectView, home_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('documents/', include(wagtaildocs_urls)),
     path("", RootRedirectView.as_view()),
 ]
 
@@ -35,4 +38,5 @@ urlpatterns += i18n_patterns(
         NewsletterSubscriberSuccessView.as_view(),
         name="newsletter_success",
     ),
+    re_path(r'', include(wagtail_urls)),
 )
