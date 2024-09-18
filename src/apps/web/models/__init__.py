ba4b8e8c3e2858a5eb5528a8b93748a8948fae79
Pere Picornell <pere@perepicornell.com>
2024-09-18 16:10:27 +0200

    Apartat notícies creat i mig funcionant.

--- src/apps/web/models/__init__.py
+++ src/apps/web/models/__init__.py
@@ -1,6 +1,8 @@
+# The contact page is in a separate app.
 from .home import HomePage
 from .settings import CustomizationSettings, AnalyticsSettings, SocialMediaIconsSettings
 from .about_us import AboutUsPage
 from .projects import ProjectListPage
 from .faq import FaqPage
 from .legal import LegalPage
+from .news import NewsListPage
--- src/apps/web/models/legal.py
+++ src/apps/web/models/legal.py
@@ -2,10 +2,10 @@ from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 
-from apps.web.models.base import BasePage
+from apps.web.models.base import BasePage, MenuLabelMixin
 
 
-class LegalPage(BasePage):
+class LegalPage(MenuLabelMixin, BasePage):
     text = RichTextField(
         _("Text"),
         features=[
