eef3eab053301e9e1292d10da8d04004994aaf97
eliashz <eliashz@gmail.com>
2024-09-19 12:32:45 +0200

    Add default e-mail to constance

--- src/project/settings.py
+++ src/project/settings.py
@@ -369,7 +369,14 @@ DJANGO_SUPERUSER_PASSWORD = env("DJANGO_SUPERUSER_PASSWORD", default=None)
 # https://django-constance.readthedocs.io/en/latest/#configuration
 CONSTANCE_BACKEND = "constance.backends.database.DatabaseBackend"
 DEFAULT_PROJECT_NAME = env.str("DEFAULT_PROJECT_NAME", default="")
-CONSTANCE_CONFIG = {"PROJECT_NAME": (DEFAULT_PROJECT_NAME, _("Project name"))}
+DEFAULT_EMAIL = env.str("DEFAULT_FROM_EMAIL", default="")
+CONSTANCE_CONFIG = {
+    "PROJECT_NAME": (DEFAULT_PROJECT_NAME, _("Project name")),
+    "DEFAULT_EMAIL": (
+        DEFAULT_EMAIL,
+        _("Email address used in case the user has no email account"),
+    ),
+}
 
 
 ################################################################################
