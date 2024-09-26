1db9e9f5e7f13cd9f890bc5ccf294d41d09e9736
MartaRF3 <60473860+MartaRF3@users.noreply.github.com>
2024-09-26 12:35:32 +0200

    Base home (#6)
    * Afegides icones
    
    * Delete 503.html
    
    * Maquetat menú mobile
    
    * #43 Maquetat header escriptori
    
    * #43 Footer mobile
    
    * #43 Footer escriptori
    
    * #21 Maquetar heading
    
    * #21 Overlay mobile
    
    * Added close button
    
    * Overlay escriptori
    
    * Afegides fonts locals
    
    * #22 Bloc call-to-action mobile
    
    * #22 Call out for desktop
    
    * Added shadow to heading
    
    * Modificat colors icones sistema
    
    * #53 Seccio documents home maquetada mobil
    
    * Max-width heading
    
    * #53 maquetada home
    
    Els apartats de projectes i notícies falten perque encara no existeixen

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,21 +14,34 @@ module.exports = {
           "200":"#bfedc7",
           "300":"#9fe5ac",
           "400":"#3fca58",
-          "500":"#31b44a",
+          "500":"#31a836", // verd base
           "600":"#2ca041",
           "700":"#27903b",
           "800":"#1f702e",
           "900":"#12401a",
           "950":"#0d3014"
         },
-        accent: {
-          "100": "#fdd360"
+        groc: {
+          "500": "#fed116", // groc base
+          "700": "#e2bd5a",
+          "900": "#a9882e"
+        },
+        blau: {
+          "300": "#028db4",
+          "500": "#028db4", // blau base
+          "700": "#164066"
+        },
+        gris: {
+          "500": "#929e9e"  // gris base
         }
       }
     },
     fontFamily: {
       'body': ['Montserrat', 'sans-serif'],
       'sans': ['Montserrat', 'sans-serif']
+    },
+    boxShadow: {
+      'video': '0 1.6px 7.2px 0 rgba(0, 0, 0, 0.21)'
     }
   },
   plugins: [
