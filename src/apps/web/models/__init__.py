458175a485ddfa0a2f2eb544e1df9efebeac7e05
Pere Picornell <pere@perepicornell.com>
2024-11-25 16:17:33 +0100

    Ruff linting and formatting.

--- src/apps/web/models/__init__.py
+++ src/apps/web/models/__init__.py
@@ -1,13 +1,17 @@
 # The contact page is in a separate app.
-from .home import HomePage as HomePage
-from .settings import (
-    CustomizationSettings as CustomizationSettings,
-    AnalyticsSettings as AnalyticsSettings,
-    SocialMediaIconsSettings as SocialMediaIconsSettings,
-)
 from .about_us import AboutUsPage as AboutUsPage
-from .projects import ProjectListPage as ProjectListPage
 from .faq import FaqPage as FaqPage
+from .home import HomePage as HomePage
 from .legal import LegalPage as LegalPage
 from .news import NewsListPage as NewsListPage
+from .projects import ProjectListPage as ProjectListPage
+from .settings import (
+    AnalyticsSettings as AnalyticsSettings,
+)
+from .settings import (
+    CustomizationSettings as CustomizationSettings,
+)
+from .settings import (
+    SocialMediaIconsSettings as SocialMediaIconsSettings,
+)
 from .snippets import FooterLogo as FooterLogo
